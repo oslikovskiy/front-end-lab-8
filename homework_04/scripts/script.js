b73b344356b938e35f091071445b11d79e37048f
Volodymyr Oslikovskyi <v.oslikovskiy@gmail.com>
2018-03-27 14:51:03 +0300

    Update script.js

--- homework_04/scripts/script.js
+++ homework_04/scripts/script.js
@@ -1,3 +1,104 @@
-// Task 1
+function assign() {
+	var obj = arguments[0];
+	
+	for (var i = 1; i < arguments.length; i++) {
+		var data = arguments[i];
+		
+		for (var key in data) {
+			obj[value] = data[value];
+		}
+	}
+	return obj;
+}
 
-// Task 2
+var defaults = {
+	width: 100,
+	height: 100
+};
+
+var options = {
+	width: 150
+};
+
+var configs = assign({}, defaults, options);
+
+function Data(name, attack, hitpoints) {
+	this.name = name;
+	this.attack = attack;
+	this.currentHitpoints = hitpoints;
+	this.totalHitpoints = hitpoints;
+}
+
+Data.prototype.getHitpoints = function () {
+	return this.currentHitpoints;
+}
+
+Data.prototype.setHitpoints = function (val) {
+	return this.currentHitpoints = val;
+}
+
+Data.prototype.getTotalHitpoints = function () {
+	return this.totalHitpoints;
+}
+
+Data.prototype.setTotalHitpoints = function (val) {
+	this.totalHitpoints = val;
+}
+
+Data.prototype.getAttack = function () {
+	return this.attack;
+}
+
+Data.prototype.setAttack = function (val) {
+	this.attack = val;
+}
+
+Data.prototype.fight = function () {
+	if (this.name == item.name) {
+		console.log('take another victim ');
+		return;
+	} else if (item.isAlive()) {
+		item.setHitpoints(item.getHitpoints() - this.getAttack())
+	} else {
+		console.log('You are dead... Fuck... ha-ha-ha');
+		return;
+	}
+}
+
+Data.prototype.isAlive = function (item) {
+	if(this.getHitpoints() > 0){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+var unit = new Data();
+
+Champion.prototype = unit;
+Monster.prototype = unit;
+
+function Champion(name, attack, hitpoints) {
+	this.name = name;
+	this.attack = attack;
+	this.hitpoints = hitpoints;
+	this.block = false;
+	this.heal = function () {
+
+	}
+	this.defence = function () {
+
+	}
+}
+
+function Monster(name, attack, hitpoints) {
+	this.name = name;
+	this.attack = attack;
+	this.hitpoints = hitpoints;
+	this.enrage = function () {
+
+	}
+	this.fury = function () {
+
+	}
+}
