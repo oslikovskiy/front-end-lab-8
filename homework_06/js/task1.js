ba57885fdde0d4b56c21a67f7eca6c059970a84c
Volodymyr Oslikovskyi <v.oslikovskiy@gmail.com>
2018-04-02 22:42:10 +0300

    add task_1 && task_2

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,46 +1,42 @@
 var http = {
-    get: function (url) {
-        return new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
+  get: function (url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
 
-            xhr.open('GET', url, true);
-            xhr.onload = function () {
-                if (this.status == 200) {
-                    resolve(this.responseText);
-                } else {
-                    var error = new Error(this.statusText);
-                    error.code = this.status;
-                    reject(error);
-                }
-            };
+      xhr.open('GET', url, true);
+      xhr.onload = function () {
+        if (this.status == 200) {
+          resolve(this.responseText);
+        } else {
+          var error = new Error(this.statusText);
+          error.code = this.status;
+          reject(error);
+        }
+      };
+      xhr.onerror = function () {
+        reject(new Error("Network Error"));
+      };
+      xhr.send();
+    });
+  },
+  post: function (url, body) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
 
-            xhr.onerror = function () {
-                reject(new Error("Network Error"));
-            };
-
-            xhr.send();
-        });
-    },
-    post: function (url) {
-        return new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            var info;
-
-            xhr.open('GET', url, true);
-            xhr.onload = function () {
-                if (this.status == 200) {
-                    resolve(this.responseText);
-                } else {
-                    var error = new Error(this.statusText);
-                    error.code = this.status;
-                    reject(error);
-                }
-            };
-            xhr.onerror = function () {
-                reject(new Error("Network Error"));
-            };
-
-            xhr.send(info);
-        });
-    }
+      xhr.open('POST', url, true);
+      xhr.onload = function () {
+        if (this.status == 200) {
+          resolve(this.responseText);
+        } else {
+          var error = new Error(this.statusText);
+          error.code = this.status;
+          reject(error);
+        }
+      };
+      xhr.onerror = function () {
+        reject(new Error("Network Error"));
+      };
+      xhr.send(body);
+    });
+  }
 }
--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,1 +1,27 @@
-;
+document.addEventListener("DOMContentLoaded", function (event) {
+    var ipForm = document.getElementById('ipForm');
+
+    ipForm.onsubmit = function (event) {
+        event.preventDefault();
+
+        var ip = document.getElementById('ip').value;
+
+        http.get('https://ipapi.co/' + ip + '/json/').then(function (response) {
+            var responseEl = document.getElementsByClassName('ip-response')[0];
+            var validateButton = document.createElement('BUTTON');
+            var postBody = response;
+
+            validateButton.appendChild(document.createTextNode("Validate"));
+            responseEl.innerHTML = response;
+            responseEl.appendChild(validateButton);
+
+            validateButton.addEventListener('click', function (event) {
+                var responseValidateEl = document.getElementsByClassName('response-validate')[0];
+
+                http.post('https://shrouded-garden-94580.herokuapp.com/', postBody).then(function (rsp) {
+                    responseValidateEl.innerHTML = rsp;
+                })
+            })
+        });
+    }
+});
